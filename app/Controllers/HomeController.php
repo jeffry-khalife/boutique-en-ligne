2893c892f177746288fbd96f8ec1db9368ee7907
jeffry-khalife <jeffry.khalife@laplateforme.io>
2025-05-09 14:30:35 +0200

    Ajout du controleur pour la page qui affiche les jeux

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,7 +4,17 @@ namespace App\Controllers;
 
 class HomeController {
     public function index() {
-        // include '../views/home.php'; 
+        $game = new \App\Models\Game();
+        $consoleFilter = $_GET['console'] ?? '';
+        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+        $page = max(1, $page);
+        $perPage = 6;
+        $offset = ($page - 1) * $perPage;
+        $games = $game->getGames($consoleFilter, $offset, $perPage);
+        $totalGames = $game->countGames($consoleFilter);
+
+        require __DIR__ . '/../Views/home.php';
     }
 }
+
 ?>
