3a82a88025263a6c9890e418cfbf0a0058525ace
jeffry-khalife <jeffry.khalife@laplateforme.io>
2025-05-09 14:30:58 +0200

    Ajout du controleur pour la page profil

--- app/Controllers/ProfilController.php
+++ app/Controllers/ProfilController.php
@@ -1,6 +1,19 @@
-<?php 
-if (!isset($_SESSION['user'])) {
-    header('Location: ?page=login');
-    exit;
+<?php
+
+namespace App\Controllers;
+
+class ProfilController {
+    public function index() {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+        if (!isset($_SESSION['user'])) {
+            header('Location: ?page=login');
+            exit;
+        }
+        require __DIR__ . '/../Views/profil.php'; 
+    }
+    
 }
+
 ?>
