bc9974e16fd86d3590691d506d0185ba0045f7f0
jeffry-khalife <jeffry.khalife@laplateforme.io>
2025-05-09 14:21:59 +0200

    Ajout du script qui va géré la connexion et l'inscription

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -69,7 +69,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 
                 if (result.success) {
                     setTimeout(() => {
-                        window.location.href = "?page=profil";
+                        window.location.href = "/boutique-en-ligne/?page=profil";
                     }, 1000);
                 }
                 
